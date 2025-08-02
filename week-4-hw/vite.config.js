c421c9a2e39497943a0088794f03698c8f221b6a
yuan6636 <aleniverson777@gmail.com>
2025-08-02 22:16:11 +0800

    第四周作業：修改 vite.config 的 base 路徑

--- week-4-hw/vite.config.js
+++ week-4-hw/vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法:
   // base: '/Repository 的名稱/'
-  base: '/hex-school/week-4-hw/',
+  base: '/hex-school/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
